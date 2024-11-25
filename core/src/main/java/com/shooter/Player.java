dcb47ebe199367edd9473ff5dd1b4ab09b32a2f6
Joe-Newm <j.newm500@gmail.com>
2024-11-24 18:29:33 -0600

    init

--- core/src/main/java/com/shooter/Player.java
+++ core/src/main/java/com/shooter/Player.java
@@ -1,4 +1,5 @@
 package com.shooter;
 
 public class Player {
+
 }
--- core/src/main/java/com/shooter/Shooter.java
+++ core/src/main/java/com/shooter/Shooter.java
@@ -1,16 +1,27 @@
 package com.shooter;
 
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Shooter extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+
+    private int VIRTUAL_WIDTH = 400;
+    private int VIRTUAL_HEIGHT = 800;
+
 
 
     @Override
